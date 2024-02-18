845a436e20e4443ea733d425be0eafd0b7caec5b
Viktor <korneevv@gmail.com>
2024-02-18 14:23:18 +0200

    done_done_portfolio_port

--- client/src/features/projects/projectsSlice.js
+++ client/src/features/projects/projectsSlice.js
@@ -1,11 +1,14 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+
+const SERVER = process.env.SERVER || "http://localhost:5050/api/projects";
+
 export const fetchProjects = createAsyncThunk(
   'projects/fetchProjects',
   async (_, { rejectWithValue }) => {
     try {
-      const response = await axios.get('http://localhost:5050/api/projects');
+      const response = await axios.get(`${SERVER}`);
       return response.data;
     } catch (error) {
       return rejectWithValue(error.message);
