4e14a639cc226ac170f1d3b6ee7648d0bcacb517
Viktor <korneevv@gmail.com>
2024-02-28 17:54:08 +0200

    client_url_send changes_ new

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // // server.js
 
+// server.js
 import express from 'express';
-import projectsRouter from './projectsRouter.js';
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 import cors from 'cors'; 
@@ -9,54 +9,48 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
 
-
+// Настройки CORS
 const corsOptions = {
     origin: process.env.REACT_APP_CLIENT_URL_SEND, 
-    optionsSuccessStatus: 200
+    optionsSuccessStatus: 200 // некоторые устаревшие браузеры (IE11, разные SmartTV) ограничены до 204
 };
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-
-
-app.use('/api/projects', projectsRouter);
-
-// Настройки nodemailer для использования с Gmail
+// Настройки nodemailer для использования с почтовым сервисом
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: process.env.EMAIL_SERVICE, // Используем переменную окружения для указания сервиса
   auth: {
-    user: process.env.EMAIL_USERNAME, // Ваш Gmail адрес
-    pass: process.env.EMAIL_PASSWORD, // Ваш пароль
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
   }
 });
 
+// Обработчик POST запроса для отправки электронной почты
 app.post('/api/send-email', (req, res) => {
   const { name, email, message } = req.body;
   
   const mailOptions = {
-    from: `"${name}" <${email}>`,
-    to: process.env.EMAIL_TO,
-    subject: 'New Contact Form Submission',
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`
+    from: `"${name}" <${email}>`, // Отправитель
+    to: process.env.EMAIL_TO, // Получатель
+    subject: 'New Contact Form Submission', // Тема письма
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`, // Текст письма
+    html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>` // HTML тело письма
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.log(error);
-      res.status(500).send('Error sending email');
+      console.error(error);
+      res.status(500).send('Error sending email'); // Ошибка при отправке
     } else {
       console.log('Email sent: ' + info.response);
-      res.status(200).send('Email sent successfully');
+      res.status(200).send('Email sent successfully'); // Успешно отправлено
     }
   });
 });
+
+// Запуск сервера
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
