6315666adc98586e87ab73e9f79d01482bceb894
Viktor <korneevv@gmail.com>
2024-02-28 16:46:46 +0200

    links

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -13,7 +13,9 @@ const ContactForm = () => {
         e.preventDefault();
         setLoading(true);
         try {
-            await axios.post('http://localhost:3001/api/send-email', { name, email, message });
+            // Используем переменную окружения для определения URL сервера
+            const serverUrl = `${process.env.REACT_APP_SERVER_URL_SEND}api/send-email`;
+            await axios.post(serverUrl, { name, email, message });
             setLoading(false);
             setSnackbarMessage('Message sent successfully');
             setSnackbarOpen(true);
@@ -28,9 +30,9 @@ const ContactForm = () => {
         }
     };
 
-    // const handleCloseSnackbar = () => {
-    //     setSnackbarOpen(false);
-    // };
+    const handleCloseSnackbar = () => {
+        setSnackbarOpen(false);
+    };
 
     return (
         <div className="max-w-md mx-auto p-6 bg-gray-100 rounded-lg shadow-lg">
@@ -77,12 +79,104 @@ const ContactForm = () => {
                     {loading ? 'Sending...' : 'Send'}
                 </button>
             </form>
-            <div className={`mt-4 p-2 bg-${snackbarOpen ? 'green' : 'red'}-500 text-white text-center rounded-md ${snackbarOpen ? 'block' : 'hidden'}`}>
-                {snackbarMessage}
-            </div>
+            {snackbarOpen && (
+                <div className={`mt-4 p-2 text-white text-center rounded-md ${snackbarOpen ? 'bg-green-500' : 'bg-red-500'}`}>
+                    {snackbarMessage}
+                    <button onClick={handleCloseSnackbar} className="text-lg ml-4">&times;</button>
+                </div>
+            )}
         </div>
     );
 };
 
 export default ContactForm;
+
+
+// import React, { useState } from 'react';
+// import axios from 'axios';
+
+// const ContactForm = () => {
+//     const [name, setName] = useState('');
+//     const [email, setEmail] = useState('');
+//     const [message, setMessage] = useState('');
+//     const [loading, setLoading] = useState(false);
+//     const [snackbarOpen, setSnackbarOpen] = useState(false);
+//     const [snackbarMessage, setSnackbarMessage] = useState('');
+
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         setLoading(true);
+//         try {
+//             await axios.post('http://localhost:3001/api/send-email', { name, email, message });
+//             setLoading(false);
+//             setSnackbarMessage('Message sent successfully');
+//             setSnackbarOpen(true);
+//             setName('');
+//             setEmail('');
+//             setMessage('');
+//         } catch (error) {
+//             console.error('Error sending message:', error);
+//             setLoading(false);
+//             setSnackbarMessage('Error sending message');
+//             setSnackbarOpen(true);
+//         }
+//     };
+
+//     // const handleCloseSnackbar = () => {
+//     //     setSnackbarOpen(false);
+//     // };
+
+//     return (
+//         <div className="max-w-md mx-auto p-6 bg-gray-100 rounded-lg shadow-lg">
+//             <h2 className="text-2xl font-bold mb-4">Contact Form</h2>
+//             <form onSubmit={handleSubmit}>
+//                 <div className="mb-4">
+//                     <label htmlFor="name" className="block text-gray-700 font-semibold mb-1">Name</label>
+//                     <input
+//                         type="text"
+//                         id="name"
+//                         value={name}
+//                         onChange={(e) => setName(e.target.value)}
+//                         required
+//                         className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+//                     />
+//                 </div>
+//                 <div className="mb-4">
+//                     <label htmlFor="email" className="block text-gray-700 font-semibold mb-1">Email</label>
+//                     <input
+//                         type="email"
+//                         id="email"
+//                         value={email}
+//                         onChange={(e) => setEmail(e.target.value)}
+//                         required
+//                         className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+//                     />
+//                 </div>
+//                 <div className="mb-4">
+//                     <label htmlFor="message" className="block text-gray-700 font-semibold mb-1">Message</label>
+//                     <textarea
+//                         id="message"
+//                         rows="4"
+//                         value={message}
+//                         onChange={(e) => setMessage(e.target.value)}
+//                         required
+//                         className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+//                     ></textarea>
+//                 </div>
+//                 <button
+//                     type="submit"
+//                     disabled={loading}
+//                     className="w-full bg-blue-500 text-white py-2 px-4 rounded-md font-semibold hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+//                 >
+//                     {loading ? 'Sending...' : 'Send'}
+//                 </button>
+//             </form>
+//             <div className={`mt-4 p-2 bg-${snackbarOpen ? 'green' : 'red'}-500 text-white text-center rounded-md ${snackbarOpen ? 'block' : 'hidden'}`}>
+//                 {snackbarMessage}
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default ContactForm;
 
--- client/src/features/Home/Home.js
+++ client/src/features/Home/Home.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import { FaLinkedin, FaGithub, FaInstagram, FaWhatsapp, FaTelegramPlane } from 'react-icons/fa';
 
 const Home = () => {
   return (
-    <div className="p-14 rounded-lg mt-2 mb-2">
-      <div className="text-center mb-2">
-        <h1 className="text-3xl font-bold mt-10">Welcome!</h1>
-        <p className="mt-4 text-left">Hello, I'm Viktor Korneev, a Full Stack Developer with experience in JavaScript, Python, and a strong background in Hi-Tech industries.</p>
-        <p className="mt-2 text-left">I'm passionate about leveraging my skills in software development to build modern, efficient web applications and contribute to innovative projects. Explore my portfolio to see my work and the technologies I specialize in.</p>
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center">
+      <div className="container mx-auto px-4">
+        {/* Header с ссылкой на скачивание и основным контентом */}
+        <header className="flex justify-between items-center py-6">
+          <h1 className="text-2xl font-bold text-gray-800">Welcome to Viktor Korneev's Portfolio</h1>
+          <a href="https://drive.google.com/file/d/103L9ZsyUULMe8GkTnfuhoZKpZi5G96-I/view?usp=sharing" className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110" download="Viktor_Korneev_Resume">Download CV</a>
+        </header>
+        {/* Основной контент */}
+        <main>
+          <p className="mt-4 text-gray-600">Hello, I'm Viktor Korneev, a Full Stack Developer with experience in JavaScript, Python, and a strong background in Hi-Tech industries.</p>
+          <p className="mt-2 text-gray-600">I'm passionate about leveraging my skills in software development to build modern, efficient web applications and contribute to innovative projects. Explore my portfolio to see my work and the technologies I specialize in.</p>
+          {/* Блок со ссылками на социальные сети */}
+          <div className="text-2xl flex justify-center space-x-4 mb-20 mt-20">
+            <a href="https://www.linkedin.com/in/viktor-korneev/" className="text-gray-800 hover:text-blue-400 transition duration-300 flex items-center"><FaLinkedin className="mr-2" />LinkedIn</a>
+            <a href="https://github.com/k0ren0" className="text-gray-800 hover:text-gray-400 transition duration-300 flex items-center"><FaGithub className="mr-2" />GitHub</a>
+            <a href="https://www.instagram.com/koren0" className="text-gray-800 hover:text-blue-300 transition duration-300 flex items-center"><FaInstagram className="mr-2" />Instagram</a>
+            <a href="https://wa.me/0534912640" className="text-gray-800 hover:text-green-500 transition duration-300 flex items-center"><FaWhatsapp className="mr-2" />WhatsApp</a>
+            <a href="https://t.me/koren0" className="text-gray-800 hover:text-blue-400 transition duration-300 flex items-center"><FaTelegramPlane className="mr-2" />Telegram</a>
+          </div>
+        </main>
       </div>
     </div>
   );
 };
 
 export default Home;
+
 
 
 
