2d72bda9a48470e058078aadffe9bbdf8461e13e
Viktor <korneevv@gmail.com>
2024-03-06 10:56:44 +0300

    update adapt Home

--- client/src/features/Home/Home.js
+++ client/src/features/Home/Home.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { FaLinkedin, FaGithub, FaInstagram, FaWhatsapp, FaTelegramPlane } from 'react-icons/fa';
 
 const Home = () => {
+  // ADAPTIVE STYLE
+  const linkStyle = "text-gray-800 hover:text-blue-400 transition duration-300 flex items-center hover:scale-110 space-x-2";
+
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center">
       <div className="container mx-auto px-4">
-        {/* Header с ссылкой на скачивание и основным контентом */}
         <header className="flex justify-between items-center py-6">
           <h1 className="text-2xl font-bold text-gray-800">Welcome to my Portfolio</h1>
           <a href="https://drive.google.com/file/d/1RsWpu7tW7P9wLGXuUn6g14YLARhC14-h/view?usp=sharing" className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110" download="Viktor_Korneev_Resume">Download CV</a>
         </header>
-        {/* Основной контент */}
         <main>
           <p className="mt-4 text-gray-600">Hello, I'm Viktor Korneev, a Full Stack Developer with experience in JavaScript, Python, and a strong background in Hi-Tech industries.</p>
           <p className="mt-2 text-gray-600">I'm passionate about leveraging my skills in software development to build modern, efficient web applications and contribute to innovative projects. Explore my portfolio to see my work and the technologies I specialize in.</p>
-          {/* Блок со ссылками на социальные сети */}
-          <div className="text-2xl flex justify-center space-x-4 mb-20 mt-20">
-            <a href="https://www.linkedin.com/in/viktor-korneev/" className="text-gray-800 hover:text-blue-400 transition duration-300 flex items-center"><FaLinkedin className="mr-2" />LinkedIn</a>
-            <a href="https://github.com/k0ren0" className="text-gray-800 hover:text-gray-400 transition duration-300 flex items-center"><FaGithub className="mr-2" />GitHub</a>
-            <a href="https://www.instagram.com/koren0" className="text-gray-800 hover:text-blue-300 transition duration-300 flex items-center"><FaInstagram className="mr-2" />Instagram</a>
-            <a href="https://wa.me/0534912640" className="text-gray-800 hover:text-green-500 transition duration-300 flex items-center"><FaWhatsapp className="mr-2" />WhatsApp</a>
-            <a href="https://t.me/koren0" className="text-gray-800 hover:text-blue-400 transition duration-300 flex items-center"><FaTelegramPlane className="mr-2" />Telegram</a>
+          <div className="flex flex-wrap justify-center gap-4 my-20">
+            <a href="https://www.linkedin.com/in/viktor-korneev/" className={linkStyle}><FaLinkedin className="mr-2" />LinkedIn</a>
+            <a href="https://github.com/k0ren0" className={linkStyle}><FaGithub className="mr-2" />GitHub</a>
+            <a href="https://www.instagram.com/koren0" className={linkStyle}><FaInstagram className="mr-2" />Instagram</a>
+            <a href="https://wa.me/0534912640" className={linkStyle}><FaWhatsapp className="mr-2" />WhatsApp</a>
+            <a href="https://t.me/koren0" className={linkStyle}><FaTelegramPlane className="mr-2" />Telegram</a>
           </div>
         </main>
       </div>
@@ -32,6 +32,33 @@ export default Home;
 
 
 
+// import React from 'react';
+// import { FaLinkedin, FaGithub, FaInstagram, FaWhatsapp, FaTelegramPlane } from 'react-icons/fa';
 
+// const Home = () => {
+//   return (
+//     <div className="min-h-screen bg-gray-100 flex flex-col justify-center">
+//       <div className="container mx-auto px-4">
+//         <header className="flex justify-between items-center py-6">
+//           <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-gray-800">Welcome to my Portfolio</h1>
+//           <a href="https://drive.google.com/file/d/1RsWpu7tW7P9wLGXuUn6g14YLARhC14-h/view?usp=sharing" className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110" download="Viktor_Korneev_Resume">Download CV</a>
+//         </header>
+//         <main>
+//           <p className="mt-4 text-gray-600 text-base sm:text-lg">Hello, I'm Viktor Korneev, a Full Stack Developer with experience in JavaScript, Python, and a strong background in Hi-Tech industries.</p>
+//           <p className="mt-2 text-gray-600 text-base sm:text-lg">I'm passionate about leveraging my skills in software development to build modern, efficient web applications and contribute to innovative projects. Explore my portfolio to see my work and the technologies I specialize in.</p>
+//           <div className="text-xl sm:text-2xl flex justify-center space-x-4 mb-20 mt-20">
+//             <a href="https://www.linkedin.com/in/viktor-korneev/" className="text-gray-800 hover:text-blue-400 transition duration-300 flex items-center hover:scale-110"><FaLinkedin className="mr-2" />LinkedIn</a>
+//             <a href="https://github.com/k0ren0" className="text-gray-800 hover:text-gray-400 transition duration-300 flex items-center hover:scale-110"><FaGithub className="mr-2" />GitHub</a>
+//             <a href="https://www.instagram.com/koren0" className="text-gray-800 hover:text-blue-300 transition duration-300 flex items-center hover:scale-110"><FaInstagram className="mr-2" />Instagram</a>
+//             <a href="https://wa.me/0534912640" className="text-gray-800 hover:text-green-500 transition duration-300 flex items-center hover:scale-110"><FaWhatsapp className="mr-2" />WhatsApp</a>
+//             <a href="https://t.me/koren0" className="text-gray-800 hover:text-blue-400 transition duration-300 flex items-center hover:scale-110"><FaTelegramPlane className="mr-2" />Telegram</a>
+//           </div>
+//         </main>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
 
 
--- client/src/features/about/About.js
+++ client/src/features/about/About.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const About = () => {
-  // Обновленный стиль для блоков с технологиями: центрирование содержимого, адаптивный отступ, и добавление тени при наведении
   const techStyle = "flex flex-col items-center justify-center p-4 hover:bg-gray-200 hover:shadow-lg rounded-lg cursor-pointer transform hover:scale-110 transition duration-300 ease-in-out";
-  // Адаптивный размер логотипа с центрированием и гармоничным распределением в зависимости от размера экрана
   const logoStyle = "h-[3rem] w-[3rem] md:h-[4rem] md:w-[4rem] lg:h-[5rem] lg:w-[5rem]";
 
   return (
@@ -13,7 +11,6 @@ const About = () => {
         <p className="text-left mt-6 mb-8">I have experience in JavaScript and Python, developed over a course and through more than four years of previous experience in the High-tech industry. I leverage strong skills in continuous self-education and effective communication to drive innovation in startup environments. I have a proven track record in project management, especially within High-tech (DLP systems) and the construction industry, where my efforts have significantly contributed to companies achieving market leadership. I would be happy to utilize my extensive professional experience and knowledge in the role of a Full Stack Developer.</p>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6 justify-items-center">
-        {/* Blocks for each technology */}
         <div className={techStyle}>
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="HTML5" className={logoStyle} />
           <span>HTML5</span>
@@ -50,7 +47,6 @@ const About = () => {
           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="Python" className={logoStyle} />
           <span>Python</span>
         </div>
-        {/* Add other technologies as needed */}
       </div>
     </div>
   );
@@ -60,61 +56,5 @@ export default About;
 
 
 
-// import React from 'react';
-
-// const About = () => {
-//   const techStyle = "flex flex-col items-center justify-center p-4 hover:bg-gray-200 rounded-lg cursor-pointer transform hover:scale-110 transition duration-300 ease-in-out space-y-2";
-//   const logoStyle = "h-[3rem] w-[3rem] md:h-[4rem] md:w-[4rem] lg:h-[5rem] lg:w-[5rem]";
-
-//   return (
-//     <div className="p-8 rounded-lg ">
-//       <div className="text-center mb-8">
-//         <h2 className="text-3xl font-bold mb-6">About Me</h2>
-//         <p className="mt-2">My experience in JavaScript and Python during course and previous experience in Hi-Tech more than 4 years, leveraging strong skills in continuous self-education and effective communication to drive innovation in startup environments. Proven track record in project management, particularly within Hi-tech (DLP systems) and the construction industry, where my efforts contributed significantly to companies achieving market leadership. I would be happy to utilize my extensive professional experience and knowledge in the role of Full Stack developer.</p>
-//       </div>
-//       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
-//         {/* Technology blocks including Python */}
-//         <div className={techStyle}>
-//           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="HTML5" className={logoStyle} />
-//           <span>HTML5</span>
-//         </div>
-//         <div className={techStyle}>
-//           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="CSS3" className={logoStyle} />
-//           <span>CSS3</span>
-//         </div>
-//         <div className={techStyle}>
-//           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="JavaScript" className={logoStyle} />
-//           <span>JavaScript</span>
-//         </div>
-//         <div className={techStyle}>
-//           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="React" className={logoStyle} />
-//           <span>React</span>
-//         </div>
-//         <div className={techStyle}>
-//           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt="Redux" className={logoStyle} />
-//           <span>Redux</span>
-//         </div>
-//         <div className={techStyle}>
-//           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="Node.js" className={logoStyle} />
-//           <span>Node.js</span>
-//         </div>
-//         <div className={techStyle}>
-//           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt="Express.js" className={logoStyle} />
-//           <span>Express.js</span>
-//         </div>
-//         <div className={techStyle}>
-//           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" alt="PostgreSQL" className={logoStyle} />
-//           <span>PostgreSQL</span>
-//         </div>
-//         <div className={techStyle}>
-//           <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="Python" className={logoStyle} />
-//           <span>Python</span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default About;
 
 
